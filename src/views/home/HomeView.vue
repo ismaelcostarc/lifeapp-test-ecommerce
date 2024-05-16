e581e867061f552bca70be0372273ca2e13c9507
Ismael Costa <ismaelac96@icloud.com>
2024-05-16 19:51:02 -0300

    feat: create home component files

---
+++ src/views/home/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import getProductsService from '@/services/products/getProducts.service'
+import HomeFilterComponent from './components/HomeFilterComponent.vue'
+import HomeSortComponent from './components/HomeSortComponent.vue'
+import HomeListComponent from './components/HomeListComponent.vue'
+
+const result = await getProductsService()
+
+console.log(result)
+</script>
+
+<template>
+  <main>
+    <HomeFilterComponent />
+
+    <HomeSortComponent />
+
+    <HomeListComponent />
+  </main>
+</template>
